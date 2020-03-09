231a29393439387c7336d289d7e6e383aeab5e34
Max <samaraanime@mail.ru>
2020-03-09 23:37:26 +0400

    doc: add comments

--- src/com/company/template_method/basic/TemplateMethodSkeleton.java
+++ src/com/company/template_method/basic/TemplateMethodSkeleton.java
@@ -1,20 +1,25 @@
 package com.company.template_method.basic;
 
+/**
+ * Template Method is a behavioral design pattern.
+ */
+
 public abstract class TemplateMethodSkeleton {
 
+//  template method, final so subclasses can't override
     final void templateMethod() {
         primitiveOperation1();
         primitiveOperation2();
         concreteOperation();
     }
-
+//  default implementation
     private void primitiveOperation1() {
         System.out.println("general operation №1");
     }
-
+//  default implementation
     private void primitiveOperation2() {
         System.out.println("general operation №2");
     }
-
+//  //methods to be implemented by subclasses
     protected abstract void concreteOperation();
 }
