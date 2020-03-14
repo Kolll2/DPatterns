0c578dcc94a77b05c05572f2d723d7a41ea44d52
Max <samaraanime@mail.ru>
2020-03-15 00:06:09 +0400

    feat: add pre menu version composite pattern

---
+++ src/com/company/сomposite/menu/MenuItem.java
@@ -0,0 +1,45 @@
+package com.company.сomposite.menu;
+
+public class MenuItem extends MenuComponent {
+    String name;
+    String description;
+    boolean vegetarian;
+    double price;
+
+    public MenuItem(String name, String description, boolean vegetarian, double price) {
+        this.name = name;
+        this.description = description;
+        this.vegetarian = vegetarian;
+        this.price = price;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public boolean isVegetarian() {
+        return vegetarian;
+    }
+
+    @Override
+    public void print() {
+        System.out.print("\t" + getName());
+        if (isVegetarian()){
+            System.out.println("(v)");
+        }
+        System.out.println(", " + getPrice());
+        System.out.println("\t" + getDescription());
+    }
+}
